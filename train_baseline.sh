dc1438135a87ceb8bd944ef78d7ff232339b28ba
maragraziani <mara.graziani@hevs.ch>
2021-08-23 10:08:09 +0200

    fix filesystem

---
+++ train_baseline.sh
@@ -0,0 +1,21 @@
+EXPERIMENT_TYPE=$1
+SEED=$2
+FILE=results/$EXPERIMENT_TYPE
+if [[ -d $FILE ]]
+then
+    read -r -p "override results in folder? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        #horovodrun -np 2 \
+        #-H localhost:4 \
+        python train_baseline.py $SEED $EXPERIMENT_TYPE
+    else
+        echo Stopping process to avoid overriding data.
+    fi
+else
+    mkdir results/$EXPERIMENT_TYPE
+    #horovodrun -np 2 \
+        #-H localhost:4 \
+        python train_baseline.py $SEED $EXPERIMENT_TYPE
+fi
+
